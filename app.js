19b201ea6dccbde2f208814b1ec25e39ba1de359
WeiWayne1030 <wayne10032004@gmail.com>
2023-02-24 11:25:04 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3001
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error')
+})
+
+db.once('open', () =>{
+  console.log('mongoDB connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('this is my rslist')

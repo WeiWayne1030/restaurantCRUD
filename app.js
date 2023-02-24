f26efa99c18db2ae6ce110e174428b8f634907bc
WeiWayne1030 <wayne10032004@gmail.com>
2023-02-24 20:35:27 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -82,6 +82,15 @@ app.post('/restaurants/:id/edit', (req, res) =>{
     .catch(err => console.log(err))
 })
 
+//刪除特定餐廳
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => { restaurant.remove() })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 
 app.listen(port, () =>{
   console.log('Express is listening on http://localhost:3001')

e65e4c937bfa36c8dbb8aca58a7f529a1b178909
WeiWayne1030 <wayne10032004@gmail.com>
2023-04-26 11:15:40 +0800

    feat: add express-session

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,13 +2,41 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 
-//登入用戶
+//登入
 router.get('/login', (req, res) =>{
   return res.render('login')
 })
 
-
 router.get('/register', (req, res) => {
   res.render('register')
 })
+
+router.post('/register', (req, res) => {
+  //輸入參數
+  const {name, email, password, confirmPassword } = req.body
+  //檢查是否註冊
+  User.findOne({ email }).then(user => {
+    //重新導向登入頁
+    if(user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果沒註冊，謝入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+  .catch(err => console.log(err))
+})
+
 module.exports = router
